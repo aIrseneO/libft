e4291e5d78028dab348f2c1f62ef91b6f940b41f
Arsene Temfack <temfack2arsene@gmail.com>
2020-06-09 21:05:00 -0700

    updated

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,50 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atemfack <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/09 20:56:49 by atemfack          #+#    #+#             */
+/*   Updated: 2020/06/09 21:02:48 by atemfack         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 int get_next_line(const int fd, char **line);
 
 int main(int argc, char **argv)
 {
-	int		fd1;
-	int		n1;
-	int		fd2;
-	int		n2;
-	char	*line1;
-	char	*line2;
+	int		i, n, fd;
+	char	*line;
 
-	if (argc != 3)
+	if (argc != 2)
 	{
-		printf("Usage: ./[binary_name] [path1] [path2]\n");
-		return (5);
+		printf("Usage: ./[binary_name] [path]\n");
+		return (-1);
 	}
-	if (((fd1 = open(argv[1], O_RDONLY)) == -1) || ((fd2 = open(argv[2], O_RDONLY)) == -1))
+	if ((fd = open(argv[1], O_RDONLY)) == -1)
 	{
 		printf("Opening failure\n");
 		return (1);
 	}
-	//int fd = open("large_file.txt.mine", O_CREAT | O_RDWR | O_TRUNC, 0755);
-	while ((n1 = get_next_line(fd1, &line1)) == 1)
+	i = 1;
+	while ((n = get_next_line(fd, &line)) > 0)
 	{
-		printf("file1: %s| %d\n", line1, n1);
-		//write(fd, line1, strlen(line1));
-		//write(fd, "\n", 1);
-		free(line1);
+		printf("line %d: %s| n = %d\n", i++, line, n);
+		free(line);
 	}
-	//close(fd);
-	while ((n2 = get_next_line(fd2, &line2)) == 1) 
-	{
-		printf("file2: %s| %d\n", line2, n2);
-		free(line2);
-	}
-	printf("<== n1 = %d, n2 = %d ==>\n", n1, n2);
-	//printf("%i == %i\n", get_next_line(fd1, &line1), get_next_line(fd2, &line2));
-	if (n1 == -1 || n2 == -1)
-		printf("Readind failure\n");
-	//free(line1);
-	//free(line2);
-	close(fd1);
-	close(fd2);
+	printf("line %d: %s| n = %d\n", i, line, n);
+	free(line);
+	printf("<== n = %d ==>\n", n);
+	close(fd);
 	return (1);
 }
