047c2b6a8bd4a4944427e72a66fc41b662630a90
arsenelefacko <temfack2arsene@gmail.com>
2021-01-25 23:17:15 -0600

    updated

--- libft/ft_isnumber.c
+++ libft/ft_isnumber.c
@@ -6,20 +6,34 @@
 /*   By: atemfack <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 16:55:07 by atemfack          #+#    #+#             */
-/*   Updated: 2021/01/25 17:03:18 by atemfack         ###   ########.fr       */
+/*   Updated: 2021/01/25 23:09:35 by atemfack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//		Check if a str is a number in between
+//	[-9223372036854775808, 9223372036854775807]
+//	Return the number of digits in the string "str"
 int	ft_isnumber(char *str)
 {
+	int		i;
+	char	sign;
+
+	sign = '+';
 	if (*str == '-' || *str == '+')
-		str++;
+		sign = *str++;
 	if (!(*str))
 		return (0);
-	while (*str)
-		if (!ft_isdigit(*str++))
+	i = 0;
+	while (str[i] && i < 19)
+		if (!ft_isdigit(str[i++]))
 			return (0);
-	return (1);
+	if (str[i])
+		return (0);
+	if (i == 19 && sign == '-' && ft_strcmp("9223372036854775808", str) < 0)
+		return (0);
+	if (i == 19 && sign == '+' && ft_strcmp("9223372036854775807", str) < 0)
+		return (0);
+	return (i);
 }
