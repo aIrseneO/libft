e8b49805ded8146752bbf1577cf774bc5e013484
arsenelefacko <temfack2arsene@gmail.com>
2021-02-23 16:33:47 -0600

    updated

--- libft/ft_strjoin2.c
+++ libft/ft_strjoin2.c
@@ -6,19 +6,17 @@
 /*   By: atemfack <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 02:09:04 by atemfack          #+#    #+#             */
-/*   Updated: 2021/01/12 22:37:31 by atemfack         ###   ########.fr       */
+/*   Updated: 2021/02/23 02:39:10 by atemfack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin2(char const *s1, char const *s2, char const *s3)
+static char *join3(char const *s1, char const *s2, char const *s3)
 {
 	size_t	i;
 	char	*s;
 
-	if (s3 == NULL)
-		return (ft_strjoin(s1, s2));
 	s = (char *)malloc(sizeof(*s) * (ft_strlen(s1) + ft_strlen(s2)
 				+ ft_strlen(s3) + 1));
 	if (s == NULL)
@@ -32,4 +30,29 @@ char	*ft_strjoin2(char const *s1, char const *s2, char const *s3)
 		s[i++] = *s3++;
 	s[i] = '\0';
 	return (s);
+}
+
+char	*ft_strjoin2(char const *s1, char const *s2, char const *s3)
+{
+	if (!s1)
+	{
+		if (!s2)
+		{
+			if (!s3)
+				return (NULL);
+			return (ft_strdup(s3));
+		}
+		if (!s3)
+			return(ft_strdup(s2));
+		return (ft_strjoin(s2, s3));
+	}
+	if (!s2)
+	{
+		if(!s3)
+			return (ft_strdup(s1));
+		return (ft_strjoin(s1, s3));
+	}
+	if (!s3)
+		return (ft_strjoin(s1, s2));
+	return (join3(s1, s2, s3));
 }
